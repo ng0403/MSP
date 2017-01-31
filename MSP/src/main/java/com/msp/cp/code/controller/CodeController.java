58cfc60e1de4f173b0cd30cb3325b582f3657bb4
Core <Core@Core-PC>
2017-01-31 15:42:12 +0900

    code

--- MSP/src/main/java/com/msp/cp/code/controller/CodeController.java
+++ MSP/src/main/java/com/msp/cp/code/controller/CodeController.java
@@ -15,13 +15,18 @@ public class CodeController {
 	@Inject
 	CodeService codeService;
 	
-	@RequestMapping(value="/list", method = RequestMethod.GET)
-	public void boardList() throws Exception{
+	/**
+	 * 업 무 명 : codeInqr 코드조회 화면
+	 * 작 성 자 : 이재욱
+     * 작 성 일 : 2017/01/31
+     * 수 정 자 : 
+     * 수 정 일 : 
+	 * 내     용 : 공통코드 및 상세코드를 조회한다. 
+	 * */
+	@RequestMapping(value="/codeInqr", method={RequestMethod.GET, RequestMethod.POST})
+	public void codeInqr() throws Exception{
 		
-		codeService.list();
-		
+		//codeService.list();
 	}
 	
-	
-
 }
--- MSP/src/main/java/com/msp/cp/code/vo/CodeVO.java
+++ MSP/src/main/java/com/msp/cp/code/vo/CodeVO.java
@@ -1,5 +1,90 @@
 package com.msp.cp.code.vo;
 
+import java.util.Date;
+
 public class CodeVO {
+	private String grp_cd, grp_nm, grp_desc, code1, code_txt, code_desc,
+	               created_by, updated_by;
+	private Date created, updated;
+	
+	public String getGrp_cd() {
+		return grp_cd;
+	}
+	
+	public void setGrp_cd(String grp_cd) {
+		this.grp_cd = grp_cd;
+	}
+	
+	public String getGrp_nm() {
+		return grp_nm;
+	}
+	
+	public void setGrp_nm(String grp_nm) {
+		this.grp_nm = grp_nm;
+	}
+	
+	public String getGrp_desc() {
+		return grp_desc;
+	}
+	
+	public void setGrp_desc(String grp_desc) {
+		this.grp_desc = grp_desc;
+	}
+	
+	public String getCode1() {
+		return code1;
+	}
+	
+	public void setCode1(String code1) {
+		this.code1 = code1;
+	}
+	
+	public String getCode_txt() {
+		return code_txt;
+	}
+	
+	public void setCode_txt(String code_txt) {
+		this.code_txt = code_txt;
+	}
+	
+	public String getCode_desc() {
+		return code_desc;
+	}
+	
+	public void setCode_desc(String code_desc) {
+		this.code_desc = code_desc;
+	}
+	
+	public String getCreated_by() {
+		return created_by;
+	}
+	
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+	
+	public String getUpdated_by() {
+		return updated_by;
+	}
+	
+	public void setUpdated_by(String updated_by) {
+		this.updated_by = updated_by;
+	}
+	
+	public Date getCreated() {
+		return created;
+	}
+	
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+	public Date getUpdated() {
+		return updated;
+	}
+	
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
 
 }
