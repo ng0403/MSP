0f9b5a80febbc34b914c64c65fb65e13f994a0c8
kh <kh@khPC>
2017-02-28 16:26:10 +0900

    11

--- MSP/src/main/java/com/msp/cp/menu/vo/MenuVO.java
+++ MSP/src/main/java/com/msp/cp/menu/vo/MenuVO.java
@@ -1,7 +1,5 @@
 package com.msp.cp.menu.vo;
 
-import java.util.ArrayList;
-
 public class MenuVO {
 	
 	public String menu_cd;
@@ -16,48 +14,10 @@ public class MenuVO {
 	public String active_key;
 	public String menu_nm_key;
 	
-	public ArrayList<MenuVO> sMenuVO;
-	public ArrayList<MenuVO> tMenuVO;
-	public ArrayList<MenuVO> fMenuVO;
-	
 	public MenuVO() {
 		super();
 	}
 	
-	public MenuVO(String menu_cd, String menu_nm, String menu_url, String menu_level, String up_menu_cd,
-			String active_flg, ArrayList<MenuVO> sMenuVO, ArrayList<MenuVO> tMenuVO,
-			ArrayList<MenuVO> fMenuVO) {
-		super();
-		this.menu_cd = menu_cd;
-		this.menu_nm = menu_nm;
-		this.menu_url = menu_url;
-		this.menu_level = menu_level;
-		this.up_menu_cd = up_menu_cd;
-		this.active_flg = active_flg;
-		this.sMenuVO = sMenuVO;
-		this.tMenuVO = tMenuVO;
-		this.fMenuVO = fMenuVO;
-	}
-
-	public ArrayList<MenuVO> getsMenuVO() {
-		return sMenuVO;
-	}
-	public void setsMenuVO(ArrayList<MenuVO> sMenuVO) {
-		this.sMenuVO = sMenuVO;
-	}
-	public ArrayList<MenuVO> gettMenuVO() {
-		return tMenuVO;
-	}
-	public void settMenuVO(ArrayList<MenuVO> tMenuVO) {
-		this.tMenuVO = tMenuVO;
-	}
-	public ArrayList<MenuVO> getfMenuVO() {
-		return fMenuVO;
-	}
-	public void setfMenuVO(ArrayList<MenuVO> fMenuVO) {
-		this.fMenuVO = fMenuVO;
-	}
-
 	public String getUp_menu_nm() {
 		return up_menu_nm;
 	}
